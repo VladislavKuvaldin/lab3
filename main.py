945c5977fe6fe360bfc39629b901ff5105cc7806
Владислав <forestmine1@gmail.com>
2025-03-07 04:27:08 +0700

    Длинные строки разделены

--- main.py
+++ main.py
@@ -74,9 +74,9 @@ class EquipmentContainer:
             parts = condition.split()  # Разделяем строку условия на части
             attribute = parts[0]  # Атрибут, по которому будет производиться сравнение
             operator = parts[1]  # Оператор сравнения
-            value = int(parts[2])  # Значение для сравнения (предполагаем, что это целое число)
-
-            attr_value = getattr(eq, attribute)  # Получаем значение атрибута у объекта оргтехники
+            value = int(parts[2])  # Значение для сравнения
+            # Получаем значение атрибута у объекта оргтехники
+            attr_value = getattr(eq, attribute)
 
             # Проверяем условие
             if operator == '<':
@@ -92,7 +92,7 @@ class EquipmentContainer:
             elif operator == '!=':
                 return attr_value != value  # Проверка на неравенство
             else:
-                raise ValueError(f"Неизвестный оператор: {operator}")  # Обработка неизвестного оператора
+                raise ValueError(f"Неизвестный оператор: {operator}")
 
         # Удаляем объекты, соответствующие условию
         self.equipments = [eq for eq in self.equipments if not check_condition(eq)]
@@ -105,28 +105,38 @@ class EquipmentContainer:
 # Функция для обработки команд из файла
 def process_commands(file_path):
     container = EquipmentContainer()  # Создаем новый контейнер для оргтехники
-
-    with open(file_path, 'r', encoding='utf-8') as file:  # Открываем файл для чтения с указанием кодировки
+    # Открываем файл для чтения с указанием кодировки
+    with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:  # Читаем файл построчно
             command = line.strip().split()  # Убираем пробелы и разбиваем строку на команды
             if command[0] == "ADD":  # Если команда "ADD"
-                data = json.loads(" ".join(command[1:]))  # Загружаем данные из JSON(все части команды кроме первой)
-                if data['type'] == 'mouse':  # Если тип мышь
-                    equipment = Mouse(data['name'], data['price'], data['last_modified'], data['mouse_type'],
-                                      data['button_count'])  # Создаем объект мыши
-                elif data['type'] == 'keyboard':  # Если тип клавиатура
-                    equipment = Keyboard(data['name'], data['price'], data['last_modified'], data['keyboard_type'],
-                                         data['connector'])  # Создаем объект клавиатуры
-                elif data['type'] == 'monitor':  # Если тип монитор
-                    equipment = Monitor(data['name'], data['price'], data['last_modified'], data['width'],
-                                        data['height'])  # Создаем объект монитора
+                # Объединяем элементы в строку
+                data = json.loads(" ".join(command[1:]))
+                if data['type'] == 'mouse':  # Создаем объект мыши
+                    equipment = Mouse(data['name'],
+                                      data['price'],
+                                      data['last_modified'],
+                                      data['mouse_type'],
+                                      data['button_count'])
+                elif data['type'] == 'keyboard':  # Создаем объект клавиатуры
+                    equipment = Keyboard(data['name'],
+                                         data['price'],
+                                         data['last_modified'],
+                                         data['keyboard_type'],
+                                         data['connector'])
+                elif data['type'] == 'monitor':  # Создаем объект монитора
+                    equipment = Monitor(data['name'],
+                                        data['price'],
+                                        data['last_modified'],
+                                        data['width'],
+                                        data['height'])
                 container.add_equipment(equipment)  # Добавляем оргтехнику в контейнер
-            elif command[0] == "REM":  # Если команда "REM"
+            elif command[0] == "REM":
                 condition = " ".join(command[1:])  # Получаем условие для удаления
                 container.remove_equipment(condition)  # Удаляем оргтехнику по условию
-            elif command[0] == "PRINT":  # Если команда "PRINT"
+            elif command[0] == "PRINT":
                 container.print_equipments()  # Выводим информацию о текущих объектах
 
-# Пример использования
+# Запускаем обработку команд из файла 'commands.txt'
 if __name__ == "__main__":
-    process_commands('commands.txt')  # Запускаем обработку команд из файла 'commands.txt'
+    process_commands('commands.txt')
